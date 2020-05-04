fbce50c80cd1e8fcf487d612bddcedf80f85bbbb
Ben Roznos <benrozos@gmail.com>
2020-05-04 10:47:42 -0700

    simplifying

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function searchToLow(){
-let search
+let country
+function searchToLow(search){
 let searchWord
 search = search.toLowerCase()
 for (let i = 0; i<search.length;i++){
@@ -11,28 +11,18 @@ for (let i = 0; i<search.length;i++){
   }
   }
 }
-chooseApi(searchWord)
+findApi(searchWord)
 }
 
 
-function chooseApi(searchWord){
-  fetch('https://api.covid19api.com/total/country/south-africa')
-    .then(r => r.json())
-    .then(data => {
-      let currentDay = data[data.length - 1]
-    }
-    if (currentDay /=== 'undefined'){
-      countryApi()
-    }
-}
-
-
-function countryApi(){
+function findApi(searchWord){
 //code for fetching from covid19api, needs to be equipt with search bar for names, names can be changed where south-africa currently populates
-fetch('https://api.covid19api.com/total/country/south-africa')
+fetch(`https://api.covid19api.com/total/country/${searchWord}`)
   .then(r => r.json())
   .then(data => {
     //curently set to find most current value in the api, however this can be changed by subtracting more from data.length to get preveous info ex data.length-2 will give object data from the day befor
     let currentDay = data[data.length - 1]
     console.log(data[data.length - 1])
   }
+country = 'south africa'
+searchToLow(country)
