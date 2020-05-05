65c389542fb71ac77c8f5b07b535a7da9da0b257
Ben Roznos <benrozos@gmail.com>
2020-05-05 14:01:57 -0700

    added charts

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function searchToLow(search) {
   console.log(newSearch)
   // calls the findApi function
   findApi(newSearch)
+  createChartInfo(newSearch)
 }
 
 
@@ -46,4 +47,66 @@ document.getElementById('searchBtn').addEventListener('click', (event) => {
   searchToLow(searchReq)
 
 })
-
+function createChartInfo(searchWord){
+  fetch(`https://api.covid19api.com/total/country/${searchWord}`)
+    .then(r => r.json())
+    .then(data => {
+      let dateArr = []
+      let confirmedArr = []
+      let recoveredArr = []
+      let deathsArr = []
+      for (i=10;i>0;i--){
+        let currentDay = data[data.length - i]
+        dateArr.push(`${currentDay.Date}`)
+        confirmedArr.push(`${currentDay.Confirmed}`)
+        recoveredArr.push(`${currentDay.Recovered}`)
+        deathsArr.push(`${currentDay.Deaths}`)
+      }
+      console.log(dateArr)
+      console.log(confirmedArr)
+      console.log(recoveredArr)
+      console.log(deathsArr)
+      makeChart(dateArr , confirmedArr , 1)
+      makeChart(dateArr, recoveredArr , 2)
+      makeChart(dateArr, deathsArr , 3)
+    })
+}
+function makeChart(xvar, yvar, chartNum){
+  var ctx = document.getElementById(`myChart${chartNum}`).getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: xvar,
+      datasets: [{
+        label: '# of Votes',
+        data: yvar,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
